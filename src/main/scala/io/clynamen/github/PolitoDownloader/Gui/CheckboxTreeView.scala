33e58c6dec9ee9717f5bf46d02d944eb485f9f15
clynamen <clynamen@gmail.com>
2014-09-22 13:28:36 +0200

    Add CheckboxTreeView

--- src/main/scala/io/clynamen/github/PolitoDownloader/Gui/CheckboxTreeView.scala
+++ src/main/scala/io/clynamen/github/PolitoDownloader/Gui/CheckboxTreeView.scala
@@ -4,7 +4,7 @@ import javafx.scene.{control => jfxsc}
 import scalafx.Includes._
 import scalafx.event.ActionEvent
 import scalafx.scene.control.{CheckBox, TreeItem, Control, TreeView}
-import scala.collection.mutable.Map
+import scala.collection.mutable.{Map, Stack}
 
 //class CheckboxTreeView[ItemView <: CheckboxTreeViewItemView[CheckboxTreeViewVisitor],
 //                        CheckboxTreeViewVisitor] private
@@ -31,6 +31,66 @@ import scala.collection.mutable.Map
       throw new Exception("Trying to add item to non existing parent")
     })
     addToTreeItem(parentTreeItem, item, treeItem)
+  }
+
+  def checkedLeaves() = getLeaves(v=>(v.graphic.value.asInstanceOf[jfxsc.CheckBox]).selected.value)
+  def uncheckedLeaves() = getLeaves(v=>(!(v.graphic.value.asInstanceOf[jfxsc.CheckBox]).selected.value))
+  def leaves() = getLeaves()
+
+  private def getLeaves(filterItem : (TreeItem[ItemView]) => (Boolean) = (_) => true) : Iterable[ItemView] = {
+
+    new Iterable[ItemView] {
+
+      def iterator = new Iterator[ItemView] {
+        val stack = Stack[scalafx.scene.control.TreeItem[ItemView]]()
+        var nextItemView : Option[ItemView] = None
+        stack.push(rootItem)
+
+        def findNext(): Option[ItemView] = {
+          while(stack.length > 0) {
+            val treeItem = stack.pop()
+            if (treeItem.children.length > 0 || !filterItem(treeItem) || (treeItem eq rootItem) ) {
+//              stack.pushAll(treeItem.children.toList)
+              for ( c <- treeItem.children) stack.push(c)
+            } else {
+              return Some(treeItem.getValue)
+            }
+          }
+          None
+        }
+
+        override def hasNext: Boolean = {
+          if(nextItemView.isEmpty) {
+            findNext match {
+              case None => false
+              case v @ Some(_) => {
+                nextItemView = v
+                true
+              }
+            }
+          } else false
+        }
+
+        override def next(): ItemView = {
+          if(nextItemView.isDefined ) {
+            val v = nextItemView.get
+            nextItemView = None
+            v
+          }
+          else {
+            if(hasNext) {
+              val v = nextItemView.get
+              nextItemView = None
+              nextItemView.get
+            }
+            else
+              throw new RuntimeException("No more items")
+          }
+        }
+
+      }
+    }
+
   }
 
   private def addToTreeItem(parent: TreeItem[ItemView], child: ItemView, childTreeItem: TreeItem[ItemView]) = {
--- src/main/scala/io/clynamen/github/PolitoDownloader/Gui/TestMainWindow.scala
+++ src/main/scala/io/clynamen/github/PolitoDownloader/Gui/TestMainWindow.scala
@@ -11,34 +11,68 @@ import scala.language.implicitConversions
 import scalafx.Includes._
 import scalafx.application.JFXApp
 import scalafx.application.JFXApp.PrimaryStage
+import scalafx.event.ActionEvent
 import scalafx.scene.Scene
 import scalafx.scene.control._
 import scalafx.scene.layout.{BorderPane, HBox, Priority, VBox}
 
 
 object TestMainWindow extends JFXApp with Logging {
-  val checkboxTreeView = CheckboxTreeView[TextItemView, TextItemViewVisitor](NullListener[TextItemView]())
+  val checkboxTreeView = CheckboxTreeView[GItemView, TextItemViewVisitor](NullListener[GItemView]())
   val item = new TextItemView
   checkboxTreeView.addItemAtRoot(item)
+  checkboxTreeView.addItem(item, new TextItemView)
+  checkboxTreeView.addItem(item, new TextItemView)
+  checkboxTreeView.addItem(item, new IconItemView)
+  checkboxTreeView.addItem(item, new IconItemView)
+  checkboxTreeView.addItem(item, new TextItemView)
+  val button = new Button() {
+    onAction = (e : ActionEvent) => {
+      var n : Integer = 0
+      for ( l <- checkboxTreeView.checkedLeaves) {
+        n = n+1
+      }
+      this.text = "changed = " + n
+    }
+  }
   stage = new PrimaryStage {
     scene = new Scene {
       stylesheets.add("Modena.css")
-      root = checkboxTreeView
+      root = new VBox {
+        content = List(checkboxTreeView , button)
+      }
     }
   }
 }
 
+trait GItemView extends CheckboxTreeViewItemView[TextItemViewVisitor] {
 
-class TextItemView extends CheckboxTreeViewItemView[TextItemViewVisitor] {
-  override def visit(visitor: TextItemViewVisitor): Unit = {}
+}
+
+class TextItemView extends GItemView {
+  override def visit(visitor: TextItemViewVisitor): Unit = visitor.visit(this)
+}
+
+class IconItemView extends GItemView {
+  override def visit(visitor: TextItemViewVisitor): Unit = visitor.visit(this)
 }
 
 class TextItemViewVisitor {
 
+  def visit(i : TextItemView): Unit = {
+
+  }
+
+  def visit(i : IconItemView): Unit = {
+
+  }
+
 }
 
 class NullListener[ItemView] extends CheckboxTreeViewListener[ItemView] {
-  override def onItemCheckedByUser(item: ItemView, checked: Boolean): Unit = {}
+  override def onItemCheckedByUser(item: ItemView, checked: Boolean): Unit = {
+    println("clicked: " + item + " " + item.getClass)
+  }
 }
 
 object NullListener {
